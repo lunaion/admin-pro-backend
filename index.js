4809a1df53e4abde606f6714b6beb7c99804fadc
Javier Luna <lunaion2200@gmail.com>
2022-01-10 12:26:57 -0500

    SPA control

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const path = require('path');
 
 const express = require('express');
 const cors = require('cors');
@@ -31,6 +32,10 @@ app.use( '/api/todo', require('./routes/busquedas') );
 app.use( '/api/login', require('./routes/auth') );
 app.use( '/api/upload', require('./routes/uploads') );
 
+/* Lo último, cualquier otra otra ruta que no sea las anteriores */
+app.get('*', (rep, res) => {
+    res.sendFile( path.resolve(__dirname, 'public/index.html') );
+});
 
 app.listen( process.env.PORT, () => {
     console.log('Servidor corriendo en el puerto ' + process.env.PORT);
